e1145e530f93af26d596008363900bc0bbba3746
husenindia <husentelwala@gmail.com>
2021-02-17 15:03:08 +0530

    Web worker demo 2
    Web worker demo 2

--- web-worker/script.js
+++ web-worker/script.js
@@ -1,13 +1,25 @@
+function processMessage(message) {
+    // Create worker    
+    var worker = new Worker('./script.js');
+    var deferred = $.Deferred();
+    var startTime = new Date().toTimeString();
+
+    // Receive
+    worker.onmessage = function(e) {
+        deferred.resolve(message + ' ' + startTime);
+    }
+
+    return deferred.promise();
+}
+
 self.onmessage = function(e) {
     var messageObject = e.data;
     var message = messageObject.message;
     sleep(2000);
-    if(messageObject.status==="Start") {
-        self.postMessage('Started');
-    } else {
-        self.postMessage(message + ' Processed');
-    }
+    self.postMessage(message + ' Processed');
 }
+
+// OPTIONAL DEMO SLEEP FUNCTION TO SET INTERVAL
 function sleep(ms) {
     var startTime = new Date().getTime();
     var stopTime = startTime + 2000;
